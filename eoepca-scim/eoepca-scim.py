4e163de4095c0253131739f3218447804f7b9048
Tiago Fernandes <tiago.fernandes@elecnor.es>
2020-04-22 15:19:36 +0000

    EOEPCA-13 method iteration

--- eoepca-scim/eoepca-scim.py
+++ eoepca-scim/eoepca-scim.py
@@ -4,41 +4,29 @@ import sys
 import base64
 
 def registerClient( targetURL, clientName, redirectURIs):
-    headers = { 'content-type': "application/json"}
-    #payload = "{ \"token_endpoint_auth_method\": \"client_credentials\", \"grant_type\" : \"client_credentials\", \"client_name\": \"" + clientName + "\", \"redirect_uris\" : ["
-    payload = "{ \"client_name\": \"" + clientName + "\", \"redirect_uris\" : ["
+    headers = { 'content-type': "application/scim+json"}
+    payload = "{ \"client_name\": \"" + clientName + "\", \"grant_types\":[\"client_credentials\"], \"redirect_uris\" : ["
     for uri in redirectURIs:
         payload += "\"" + uri.strip() + "\", "
     payload = payload[:-2]
     payload += "]}"
     res = requests.post(targetURL, data=payload, headers=headers, verify=False)
-    print(res)
     matrix = res.json()
-    print(matrix)
-    print(matrix['grant_types'])
     return (matrix)
 
 def getAccessToken( token_endpoint, credentials, redirectURIs):
     headers = { 'content-type': "application/x-www-form-urlencoded", 'Authorization' : credentials }
-    #headers = { 'content-type': "application/x-www-form-urlencoded" }
     payload = {'grant_type' : 'client_credentials'}
-    #payload = {'grant_type' : 'authorization_code'}
 
     res = requests.post(token_endpoint, headers=headers, data=payload, verify=False)
     matrix = res.json()
-    #print(matrix)
     return matrix['access_token']
-    #return matrix
 
 def createCredentials( client_id, client_secret):
     message = client_id + ':' + client_secret
-    #print(message)
     message_bytes = message.encode('utf-8')
-    #print(message_bytes)
     base64_bytes = base64.b64encode(message_bytes)
-    #print(base64_bytes)
     base64_message = base64_bytes.decode('utf-8')
-    #print(base64_message)
     credentials = 'Basic ' + base64_message
     return credentials
 
@@ -50,7 +38,6 @@ def getUserInum(userID, scimUserEndpoint, token):
     query = "userName eq \"" + userID +"\""
     payload = { 'filter' : query }
     res = requests.get(scimUserEndpoint, headers=headers, params=payload, verify=False)
-    print(res.json())
     user = (res.json())['Resources']
     return user[0]['id']
 
@@ -65,37 +52,31 @@ def addUserAttribute( inum, attributePath, newValue, scimUserEndpoint, token):
     headers = { 'content-type': "application/scim+json", 'Authorization' : createBearerToken(token) }
     operation = "{ \"op\":\"add\", \"path\": \"" + attributePath + "\", \"value\":\"" + newValue + "\"}"
     payload = "{ \"Operations\" : [" + operation + "]}"
-    print(payload)
     res = requests.patch(url, data=payload, headers=headers, verify=False)
-    print(res)
-    return
+    return res.json()
 
 def editUserAttribute( inum, attributePath, newValue, scimUserEndpoint, token):
     url = scimUserEndpoint + "/" + inum
     headers = { 'content-type': "application/scim+json", 'Authorization' : createBearerToken(token) }
     operation = "{ \"op\":\"replace\", \"path\": \"" + attributePath + "\", \"value\":\"" + newValue + "\"}"
     payload = "{ \"Operations\" : [" + operation + "]}"
-    print(payload)
     res = requests.patch(url, data=payload, headers=headers, verify=False)
-    print(res)
-    return
+    return res.json()
 
 def removeUserAttribute( inum, attributePath, scimUserEndpoint, token):
     url = scimUserEndpoint + "/" + inum
     headers = { 'content-type': "application/scim+json", 'Authorization' : createBearerToken(token) }
     operation = "{ \"op\":\"remove\", \"path\": \"" + attributePath + "\"}"
     payload = "{ \"Operations\" : [" + operation + "]}"
-    print(payload)
     res = requests.patch(url, data=payload, headers=headers, verify=False)
-    print(res)
-    return
+    return res.json()
 
 #a = registerClient("https://demoexample.gluu.org/oxauth/restv1/register", "TestClientGluu", ["https://demoexample.gluu.org/identity/authentication/getauthcode"])
 #credentials = createCredentials(a['client_id'], a['client_secret'])
-credentials = createCredentials('@!C28A.A0EC.7CA4.6154!0001!94C2.0974!0008!DF1E.BCB8.1E0E.91AE', 'secret')
-token = getAccessToken("https://demoexample.gluu.org/oxauth/restv1/token", credentials, ["https://demoexample.gluu.org/identity/authentication/getauthcode"])
-inum = getUserInum('tiago@test.com', 'https://demoexample.gluu.org/identity/restv1/scim/v2/Users', token)
-print(getUserAttributes(inum, 'https://demoexample.gluu.org/identity/restv1/scim/v2/Users', token))
+#credentials = createCredentials('@!C28A.A0EC.7CA4.6154!0001!94C2.0974!0008!DF1E.BCB8.1E0E.91AE', 'secret')
+#token = getAccessToken("https://demoexample.gluu.org/oxauth/restv1/token", credentials, ["https://demoexample.gluu.org/identity/authentication/getauthcode"])
+#inum = getUserInum('tiago@test.com', 'https://demoexample.gluu.org/identity/restv1/scim/v2/Users', token)
+#print(getUserAttributes(inum, 'https://demoexample.gluu.org/identity/restv1/scim/v2/Users', token))
 #print(editUserAttribute(inum, "name.familyName", "M Fernandes", 'https://demoexample.gluu.org/identity/restv1/scim/v2/Users', token))
 #print(addUserAttribute(inum, "name.middleName", "M", 'https://demoexample.gluu.org/identity/restv1/scim/v2/Users', token))
 #print(removeUserAttribute(inum, "name.middleName", 'https://demoexample.gluu.org/identity/restv1/scim/v2/Users', token))
