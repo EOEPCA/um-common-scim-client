7e81d6dbce7c136965043a864eef3fee204e444a
Angel Lozano <angel.lozano@elecnor.es>
2020-04-27 12:59:55 +0000

    fix naming of library

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-  name = 'eoepca-scim',
+  name = 'eoepca_scim',
   version = '1.0',
   author = 'EOEPCA',
   author_email = 'eoepca.systemteam@telespazio.com',
