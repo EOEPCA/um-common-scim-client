c2d67faff4bde1c6c1e259b410d9d1716b96f0ba
Angel Lozano <angel.lozano@elecnor.es>
2020-04-27 13:14:48 +0000

    fix version to 2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name = 'eoepca-scim',
-  version = '1.0',
+  version = '2.0',
   author = 'EOEPCA',
   author_email = 'eoepca.systemteam@telespazio.com',
   description = 'Python library to interact with SCIM protocol',
