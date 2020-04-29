569950fd634817d8300832b31b20703c06e2d626
Tiago Fernandes <tiago.fernandes@elecnor.es>
2020-04-29 18:36:20 +0000

    v2.3.3 patch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name = 'eoepca-scim',
-  version = '2.3.2',
+  version = '2.3.3',
   author = 'EOEPCA',
   author_email = 'eoepca.systemteam@telespazio.com',
   description = 'Python library to interact with SCIM protocol',
