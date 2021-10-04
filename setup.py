67b86e36666e7a6ed50c226669716af3eb5edfe0
AlvaroVillanueva <alvaro.villanueva@deimos-space.com>
2021-10-04 14:19:06 +0000

    Update gh_action when merge

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name = 'eoepca-scim',
-  version = '2.7.9',
+  version = '2.8.0',
   author = 'EOEPCA',
   author_email = 'eoepca.systemteam@telespazio.com',
   description = 'Python library to interact with SCIM protocol',
